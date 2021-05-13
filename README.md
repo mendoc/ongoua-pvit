78767865244a1f5fc7dd8e0690a31bd730c7fb97
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2021-05-13 23:52:39 +0000

    validate parameters

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # ongoua-pvit
 A npm package for use PVit payment gateway
+
+## Installation
+
+Using npm:
+```shell
+$ npm i ongoua-pvit
+```
+
+Using yarn:
+```shell
+$ yarn add ongoua-pvit
+```
+## Example
+```js
+const { OngouaPvit } = require("ongoua-pvit");
+
+const TEL_MARCHAND = process.env.TEL_MARCHAND;
+const TOKEN        = process.env.TOKEN;
+
+const clientPvit = new OngouaPvit(TEL_MARCHAND, TOKEN);
+
+clientPvit.tel_client   = "074567890";
+clientPvit.montant      = 2000;
+clientPvit.ref          = "PROD2X3T8";
+
+clientPvit.send().then((response) => {
+    const data = response.json;
+    if (data) {
+        // Do something with data
+        console.log(data.statut);
+        console.log(data.message);
+    }
+}).catch((error) => {
+    console.log("Error handling");
+    console.log(error);
+});
+```
+## Report bug
+DM me on Twitter [@DimitriONGOUA](https://twitter.com/DimitriOngoua)
--- index.js
+++ index.js
@@ -3,18 +3,61 @@ const axios = require('axios');
 const parser = require('xml2json');
 
 const PVIT_URL = "https://mypvit.com/pvit-secure-full-api.kk";
+const actionValues = [1, 2, 3, 5];
+const serviceValues = ["REST", "WEB"];
+const operateurValues = ["AM", "MC"];
 
 exports.OngouaPvit = class OngouaPvit {
-    constructor() {
-        this.tel_marchand = "";
-        this.montant = "";
+    constructor(tel_marchand, token) {
+        this.tel_marchand = tel_marchand;
+        this.montant = 0;
         this.ref = "";
         this.tel_client = "";
-        this.token = "";
+        this.token = token;
         this.action = 1;
         this.service = "REST";
         this.operateur = "AM";
         this.agent = "OngouaPvit";
+    }
+
+    validate() {
+        return new Promise((resolve, reject) => {
+            if (!this.tel_marchand)
+                reject("'tel_marchand' parameter is required");
+            else if (!this.montant)
+                reject("'montant' parameter is required");
+            else if (!this.ref)
+                reject("'ref' parameter is required");
+            else if (!this.tel_client)
+                reject("'tel_client' parameter is required");
+            else if (!this.action)
+                reject("'action' parameter is required");
+            else if (!this.service)
+                reject("'service' parameter is required");
+            else if (!this.operateur)
+                reject("'operateur' parameter is required");
+            else if (!this.isValidNumber(this.tel_marchand))
+                reject("tel_marchand is not a correct number");
+            else if (!this.isValidNumber(this.tel_client))
+                reject("tel_client is not a correct number");
+            else if (!this.ref)
+                reject("ref parameter is required");
+            else if (this.ref.length > 13)
+                reject("the 'ref' value must be 13 characters maximum");
+            else if (!actionValues.includes(this.action))
+                reject("the 'action' value is incorrect. Possible values are : " + actionValues.join(", "));
+            else if (!serviceValues.includes(this.service))
+                reject("the 'service' value is incorrect. Possible values are : " + serviceValues.join(", "));
+            else if (!operateurValues.includes(this.operateur))
+                reject("the 'operateur' value is incorrect. Possible values are : " + operateurValues.join(", "));
+            else if (parseInt(this.montant) < 100 || parseInt(this.montant) > 490000)
+                reject("the 'montant' value must be between 100 and 490000");
+            else resolve();
+        })
+    }
+
+    isValidNumber(number) {
+        return (number && number.length === 9 && (/(0){1}([6-7]){1}([0-9]){7}/).test(number));
     }
 
     lowerKeys(obj) {
@@ -26,16 +69,19 @@ exports.OngouaPvit = class OngouaPvit {
     }
 
     send() {
-        return axios.post(PVIT_URL, stringify({ ...this })).then((res) => {
+        return this.validate().then(() => {
+            return axios.post(PVIT_URL, stringify({ ...this })).then((res) => {
+                try {
+                    const resPvit = parser.toJson(res.data, { object: true });
+                    res.json = this.lowerKeys(resPvit.REPONSE);
+                } catch (e) {
+                    console.log(e);
+                }
 
-            try {
-                const resPvit = parser.toJson(res.data, { object: true });
-                res.json = this.lowerKeys(resPvit.REPONSE);
-            } catch (e) {
-                console.log(e);
-            }
-
-            return res
+                return res
+            });
         });
+
+
     }
 }
